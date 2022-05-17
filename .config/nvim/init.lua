b2812204d2def338b4041f5d8b3cd2683c6d4453
Anthony Guiri <exastir@icloud.com>
2022-05-17 09:04:51 +0200

    style(nvim): comment init.lua to have a clear list

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -1,12 +1,20 @@
 require "user.options"
 require "user.keymaps"
 require "user.plugins"
+
+-- Themes parameters
 require "user.colorscheme"
+
+-- Plugins parameters
 require "user.cmp"
 require "user.lsp"
 require "user.telescope"
 require "user.treesitter"
 require "user.autopairs"
+
+-- 42-plugins parameters
 require "user.header"
 require "user.norminette"
+
+-- Git-plugins parameters
 require "user.gitsigns"
